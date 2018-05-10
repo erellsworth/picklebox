937a220c9925c6df756f867b474cc484d0dd40c7
Edward <edward.ellsworth@gmail.com>
2018-05-09 20:15:56 -0700

    app_vars file and add redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import 'bulma/css/bulma.css'
+
+import * as app_vars from './app_vars.js';
+
+const api_options = {
+        headers: {
+          'Authorization': 'Bearer ' + app_vars.api_key,
+          'Accept': 'application/json',
+          'Content-Type' : 'application/json',
+        }
+      };
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {url: 'https://medium.com/pickle-fork'};
+    this.state = {
+      url: 'https://medium.com/pickle-fork',
+      errors:[],
+      medium_payload:{}
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,8 +30,23 @@ class App extends Component {
     this.setState({url: e.target.value});
   }
 
-  handleSubmit(){
+  handleSubmit(e){
+    e.preventDefault();
 
+    axios.get(app_vars.api_url + this.state.url.replace(/\//g, '^^'), api_options)
+          .then(function (response) {
+            console.log('response', response);
+            this.state.medium_payload = response.data.payload;
+/*            var newState = Object.assign(this.state, {
+              medium_payload:response.data.payload
+            });*/
+            this.setState(this.state);
+          })
+          .catch(function (error) {
+            console.log('error', error);
+            this.state.errors.push(error);
+            this.setState(this.state);           
+          });
   }
 
   render() {
@@ -27,7 +57,7 @@ class App extends Component {
             <header className="App-header">
               <h1 className="App-title">Welcome to Pickle Box</h1>
             </header>
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <div className="field is-horizontal">
                 <div className="field-label is-normal">
                   <label className="label">Medium URL:</label>
@@ -46,7 +76,7 @@ class App extends Component {
                   </div>
                 </div>
               </div>    
-              <button className="button is-primary">Do the thing.</button>          
+              <button className="button is-primary">Pickle My Publication</button>          
             </form>   
           </div>   
         </section>  
